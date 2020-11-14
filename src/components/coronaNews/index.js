4c01698732b9fafa43819c35c12d0fb3050c437c
rafif113 <rafifyusuf@gmail.com>
2020-11-14 17:29:58 +0700

    final

--- src/components/coronaNews/index.js
+++ src/components/coronaNews/index.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import app from '../../services/firebase';
 import Pagination from './pagination';
 import 'firebase/database';
+import { Link } from 'react-router-dom';
 
 const CoronaNews = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(9);
-  const [newsPerPage, setNewsPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [newsPerPage, setNewsPerPage] = useState(3);
 
   useEffect(() => {
     const db = app.database().ref('news');
@@ -24,7 +25,10 @@ const CoronaNews = () => {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  console.log(news);
+  const formatDate = (string) => {
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(string).toLocaleDateString([], options);
+  };
 
   if (isLoading) {
     return <h2>Loading.....</h2>;
@@ -37,15 +41,23 @@ const CoronaNews = () => {
           <>
             <div class="card text-center" key={note.id}>
               <div class="card-header">
-                <h5 class="card-title">{note.activity[0].title}</h5>
+                <Link to={`/infoCorona/${note.date}`}>
+                  <h5 class="card-title">{formatDate(note.date)}</h5>
+                </Link>
               </div>
-              <div class="card-body">
-                <p class="card-text">{note.activity[0].desc}</p>
-                <a href={note.activity[0].url} class="btn btn-link">
-                  Link Berita
-                </a>
-              </div>
-              <div class="card-footer text-muted">{note.date}</div>
+              {note.activity.map((data) => {
+                return (
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">{data.title}</h5>
+                      <p class="card-text">{data.desc}</p>
+                      <a href={data.url} class="card-link">
+                        link
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
             <br></br>
           </>
---
+++ src/pages/detailDate/index.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import app from '../../services/firebase';
+import 'firebase/database';
+
+const formatDate = (string) => {
+  var options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(string).toLocaleDateString([], options);
+};
+
+const NewsItem = ({ news }) => {
+  const { date, activity } = news[0];
+  return (
+    <div class="card text-center">
+      <div class="card-header">
+        <h5 class="card-title">{formatDate(date)}</h5>
+      </div>
+      {activity.map((data) => {
+        return (
+          // <div key={data.url}>
+          //   <a href={data.url} target="blank">
+          //     <h5>{data.title}</h5>
+          //   </a>
+          //   <p>{data.desc}</p>
+          // </div>
+
+          <div class="card" key={data.url}>
+            <div class="card-body">
+              <a href={data.url} class="card-link" target="blank">
+                <h5 class="card-title">{data.title}</h5>
+              </a>
+              <p class="card-text">{data.desc}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const DetailDate = () => {
+  const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const params = useParams();
+  const { dateId } = params;
+
+  useEffect(() => {
+    const db = app.database().ref('news');
+    db.on('value', (snapshot) => {
+      const firebaseNews = snapshot.val();
+      const filterByDate = firebaseNews.data.filter((resp) => {
+        return resp.date === dateId;
+      });
+      setNews(filterByDate);
+      setIsLoading(false);
+    });
+  }, [dateId]);
+
+  if (isLoading) {
+    return <h2 className="container">Loading.....</h2>;
+  }
+
+  return (
+    <div className="container">
+      <h1>Info Corona</h1>
+      {!isLoading && news.length > 0 ? (
+        <NewsItem news={news} />
+      ) : (
+        <p>Loading</p>
+      )}
+    </div>
+  );
+};
+
+export default DetailDate;
